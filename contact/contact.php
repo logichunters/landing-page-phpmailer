df711f69982b7d9251050dbed4f06efbc55d0ce7
sasadra <sasaworkpro@gmail.com>
2022-09-19 20:16:40 -0700

    Implemented contact form emailing with phpmailer

---
+++ contact/contact.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
+*/
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require './PHPMailer-master/vendor/autoload.php';
+
+/*
+*  CONFIGURE EVERYTHING HERE
+*/
+
+// an email address that will be in the From field of the email.
+$fromEmail = $_POST['email'];
+$fromName = $_POST['name'];
+
+// an email address & name that will receive the email with the output of the form
+$sendToEmail = 'name@domain.com';
+$sendToName = 'Name';
+
+// subject of the email
+$subject = 'New message from contact form';
+
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name:', 'email' => 'Email:', 'message' => 'Message:');
+
+// message that will be displayed when everything is OK :)
+$okMessage = 'Successfully submitted - we will get back to you soon!';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+/*
+*  LET'S DO THE SENDING
+*/
+
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
+
+try
+{
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+    $emailTextHtml = "<h3>New message from the w3newbie Theme:</h3><hr>";
+    $emailTextHtml .= "<table>";
+
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email
+        if (isset($fields[$key])) {
+            $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+        }
+    }
+    $emailTextHtml .= "</table><hr>";
+    $emailTextHtml .= "<p>Have a great day!<br><br>Sincerely,<br><br>w3newbie Theme</p>";
+
+    $mail = new PHPMailer();
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 2;  // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true;  // authentication enabled
+    $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
+    $mail->SMTPAutoTLS = false;
+    $mail->Host = '';
+    $mail->Port = 587; // port_number here.
+    $mail->Username = "username_smtp";
+    $mail->Password = "password_smtp";
+
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+    $mail->setFrom($fromEmail, $fromName);
+    $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
+    $mail->addReplyTo($_POST['email'], $_POST['name']);
+    $mail->Subject = $subject;
+    $mail->Body = $emailTextHtml;
+    $mail->isHTML(true);
+
+    if(!$mail->send()) {
+        throw new \Exception('Email send failed. ' . $mail->ErrorInfo);
+    }
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+}
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
+?>
